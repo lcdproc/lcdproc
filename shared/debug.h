91c430a8c4075ebb31150c9cc7729b9a72264ebc
gfk <gfk@users.sourceforge.net>
2001-11-11 19:18:28 +0000

    Fixed to remove the warnings when debugging is turned on.

--- shared/debug.h
+++ shared/debug.h
@@ -2,17 +2,37 @@
 #define DEBUG_H
 
 #ifdef HAVE_CONFIG_H
-#include "config.h"
+#include <config.h>
 #endif
+
+/* DEBUGGING
+To enable the debug() function on all of the software, just type:
+./configure --enable-debug
+and recompile with 'make'
+
+To enable the debug() function only in specific files:
+1) Configure without enabling debug (that is without --enable-debug)
+2) Edit the source file that you want to debug and put this:
+#define DEBUG
+#include "shared/debug.h"
+#undef DEBUG
+Then recompile with 'make'
+This way, the global DEBUG macro is off but is locally enabled in 
+certains parts of the software.
+*/
 
 #ifdef DEBUG
-
-#define debug printf
-
-#else
-
-#define debug						  /* printf */
-
+#include <stdarg.h>
+#include <stdio.h>
 #endif
+
+static inline void debug(const char *format, .../*args*/) {
+#ifdef DEBUG
+  va_list ap;
+  va_start(ap, format);
+  vfprintf(stderr, format, ap);
+  va_end(ap);
+#endif /*DEBUG*/
+}
 
 #endif
