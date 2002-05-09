c730e2dba1a8114ed5dd8ddf6c6d8f5581fd3127
gfk <gfk@users.sourceforge.net>
2002-05-09 00:30:19 +0000

    Solaris portability fixes.

--- server/drivers/timing.h
+++ server/drivers/timing.h
@@ -76,6 +76,44 @@
 # include <sched.h>
 #endif
 
+/* Convenience macros for operations on timevals.
+   NOTE: `timercmp' does not work for >= or <=.  */
+#ifndef timerisset
+# define timerisset(tvp)        ((tvp)->tv_sec || (tvp)->tv_usec)
+#endif
+#ifndef timerclear
+# define timerclear(tvp)        ((tvp)->tv_sec = (tvp)->tv_usec = 0)
+#endif
+#ifndef timercmp
+# define timercmp(a, b, CMP)                                                  \
+  (((a)->tv_sec == (b)->tv_sec) ?                                             \
+   ((a)->tv_usec CMP (b)->tv_usec) :                                          \
+   ((a)->tv_sec CMP (b)->tv_sec))
+#endif
+#ifndef timeradd
+# define timeradd(a, b, result)                                               \
+  do {                                                                        \
+    (result)->tv_sec = (a)->tv_sec + (b)->tv_sec;                             \
+    (result)->tv_usec = (a)->tv_usec + (b)->tv_usec;                          \
+    if ((result)->tv_usec >= 1000000)                                         \
+      {                                                                       \
+        ++(result)->tv_sec;                                                   \
+        (result)->tv_usec -= 1000000;                                         \
+      }                                                                       \
+  } while (0)
+#endif
+#ifndef timersub
+# define timersub(a, b, result)                                               \
+  do {                                                                        \
+    (result)->tv_sec = (a)->tv_sec - (b)->tv_sec;                             \
+    (result)->tv_usec = (a)->tv_usec - (b)->tv_usec;                          \
+    if ((result)->tv_usec < 0) {                                              \
+      --(result)->tv_sec;                                                     \
+      (result)->tv_usec += 1000000;                                           \
+    }                                                                         \
+  } while (0)
+#endif
+
 /////////////////////////////////////////////////////////////////
 // Initialisation
 //
