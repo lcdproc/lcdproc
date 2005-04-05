bd8310d8b20b1a81bdf00277e71901cf027223b9
marschap <marschap@users.sourceforge.net>
2005-04-05 07:25:52 +0000

    bugfix: make essential_clock_screen() available in clients/lcdproc/mode.c (VB)

--- clients/lcdproc/chrono.h
+++ clients/lcdproc/chrono.h
@@ -5,5 +5,6 @@ int clock_screen (int rep, int display);
 int uptime_screen (int rep, int display);
 int time_screen (int rep, int display);
 int big_clock_screen (int rep, int display);
+int essential_clock_screen (int rep, int display);
 
 #endif
