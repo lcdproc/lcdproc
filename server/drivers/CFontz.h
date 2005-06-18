552f388c6e8539264d0a298f71edf79681a4f3f5
marschap <marschap@users.sourceforge.net>
2005-06-18 13:06:57 +0000

    space cleanup

--- server/drivers/CFontz.h
+++ server/drivers/CFontz.h
@@ -13,23 +13,23 @@
 #define DEFAULT_SIZE "20x4"
 
 
-MODULE_EXPORT int  CFontz_init (Driver * drvthis, char *device);
-MODULE_EXPORT void CFontz_close (Driver * drvthis);
-MODULE_EXPORT int  CFontz_width (Driver * drvthis);
-MODULE_EXPORT int  CFontz_height (Driver * drvthis);
-MODULE_EXPORT void CFontz_clear (Driver * drvthis);
-MODULE_EXPORT void CFontz_flush (Driver * drvthis);
-MODULE_EXPORT void CFontz_string (Driver * drvthis, int x, int y, char string[]);
-MODULE_EXPORT void CFontz_chr (Driver * drvthis, int x, int y, char c);
+MODULE_EXPORT int  CFontz_init (Driver *drvthis, char *device);
+MODULE_EXPORT void CFontz_close (Driver *drvthis);
+MODULE_EXPORT int  CFontz_width (Driver *drvthis);
+MODULE_EXPORT int  CFontz_height (Driver *drvthis);
+MODULE_EXPORT void CFontz_clear (Driver *drvthis);
+MODULE_EXPORT void CFontz_flush (Driver *drvthis);
+MODULE_EXPORT void CFontz_string (Driver *drvthis, int x, int y, unsigned char string[]);
+MODULE_EXPORT void CFontz_chr (Driver *drvthis, int x, int y, unsigned char c);
 
-MODULE_EXPORT void CFontz_vbar (Driver * drvthis, int x, int y, int len, int promille, int options);
-MODULE_EXPORT void CFontz_hbar (Driver * drvthis, int x, int y, int len, int promille, int options);
-MODULE_EXPORT int  CFontz_icon(Driver * drvthis, int x, int y, int icon);
+MODULE_EXPORT void CFontz_vbar (Driver *drvthis, int x, int y, int len, int promille, int options);
+MODULE_EXPORT void CFontz_hbar (Driver *drvthis, int x, int y, int len, int promille, int options);
+MODULE_EXPORT int  CFontz_icon(Driver *drvthis, int x, int y, int icon);
 
-MODULE_EXPORT void CFontz_set_char (Driver * drvthis, int n, char *dat);
+MODULE_EXPORT void CFontz_set_char (Driver *drvthis, int n, char *dat);
 
-MODULE_EXPORT int  CFontz_get_contrast (Driver * drvthis);
-MODULE_EXPORT void CFontz_set_contrast (Driver * drvthis, int contrast);
-MODULE_EXPORT void CFontz_backlight (Driver * drvthis, int on);
+MODULE_EXPORT int  CFontz_get_contrast (Driver *drvthis);
+MODULE_EXPORT void CFontz_set_contrast (Driver *drvthis, int contrast);
+MODULE_EXPORT void CFontz_backlight (Driver *drvthis, int on);
 
 #endif
