d9999ff9d34a6899835764b35668274693f654da
marschap <marschap@users.sourceforge.net>
2006-03-29 06:34:41 +0000

    updates in preparation of version 0.5

--- scripts/nightly/generate-nightlies.sh
+++ scripts/nightly/generate-nightlies.sh
@@ -4,7 +4,7 @@ SSH=/usr/bin/ssh
 MAKENIGHTLY=${NIGHTLYDIR}/make-nightly.sh
 #####
 
-for branch in current stable-0-4-x; do
+for branch in current stable-0-5-x; do
 	${MAKENIGHTLY} $branch
 	${SSH} -q lcdproc.sourceforge.net "sh lcdproc-finish-nightly.sh $branch"
 done
