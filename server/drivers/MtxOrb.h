d76637c771d637562c465e5419b1e7f78c995d6c
marschap <marschap@users.sourceforge.net>
2006-09-29 21:15:45 +0000

    remove unused stuff; reorganize a bit

--- server/drivers/MtxOrb.h
+++ server/drivers/MtxOrb.h
@@ -3,6 +3,14 @@
 
 #include "lcd.h"
 
+#define DEFAULT_CONTRAST	480
+#define DEFAULT_DEVICE		"/dev/lcd"
+#define DEFAULT_SPEED		19200
+#define DEFAULT_LINEWRAP	1
+#define DEFAULT_AUTOSCROLL	1
+#define DEFAULT_CURSORBLINK	0
+
+
 MODULE_EXPORT int  MtxOrb_init (Driver *drvthis);
 MODULE_EXPORT void MtxOrb_close (Driver *drvthis);
 MODULE_EXPORT int  MtxOrb_width (Driver *drvthis);
@@ -27,24 +35,6 @@ MODULE_EXPORT void MtxOrb_num (Driver *drvthis, int x, int num);
 MODULE_EXPORT int  MtxOrb_icon (Driver *drvthis, int x, int y, int icon);
 MODULE_EXPORT void MtxOrb_vbar (Driver * drvthis, int x, int y, int len, int promille, int options);
 MODULE_EXPORT void MtxOrb_hbar (Driver * drvthis, int x, int y, int len, int promille, int options);
-
-
-#define DEFAULT_CONTRAST	480
-#define DEFAULT_DEVICE		"/dev/lcd"
-#define DEFAULT_SPEED		19200
-#define DEFAULT_LINEWRAP	1
-#define DEFAULT_AUTOSCROLL	1
-#define DEFAULT_CURSORBLINK	0
-
-/* These are the keys for a (possibly) broken LK202-25...*/
-#define KEY_UP    'I'
-#define KEY_DOWN  'F'
-#define KEY_LEFT  'K'
-#define KEY_RIGHT 'A'
-#define KEY_F1    'N'
-/* TODO: add more if you've got any more ;) or correct the settings
- * the actual translation is done in MtxOrb_get_key()
- */
 
 #endif
 
