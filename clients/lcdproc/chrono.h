2a5bf194dfb4ba448374dd7abba383e00044dfe1
mmdolze <mmdolze@users.sourceforge.net>
2011-11-20 15:31:18 +0000

    Fix previous commit

---
+++ clients/lcdproc/chrono.h
@@ -0,0 +1,10 @@
+#ifndef CHRONO_H
+#define CHRONO_H
+
+int clock_screen(int rep, int display, int *flags_ptr);
+int uptime_screen(int rep, int display, int *flags_ptr);
+int time_screen(int rep, int display, int *flags_ptr);
+int big_clock_screen(int rep, int display, int *flags_ptr);
+int mini_clock_screen(int rep, int display, int *flags_ptr);
+
+#endif
