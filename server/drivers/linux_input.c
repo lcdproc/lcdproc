d68c7e4da03362795edef12373cc80b3e553e29c
Hans de Goede <hdegoede@redhat.com>
2019-06-06 19:21:56 +0200

    server/drivers/linux_input: Add support for opening the device by name
    The numbering of /dev/input/event# nodes depends on probe ordering and as
    such is not stable.
    
    Add support for opening an input-device by its name. If multiple devices
    with the same name exists, the first one which matches will be used.
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>

--- server/drivers/linux_input.c
+++ server/drivers/linux_input.c
@@ -3,12 +3,14 @@
  * subsystem of the linux kernel..
  */
 
+#include <dirent.h>
 #include <errno.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/ioctl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -77,6 +79,60 @@ MODULE_EXPORT int stay_in_foreground = 0;
 MODULE_EXPORT int supports_multiple = 1;
 MODULE_EXPORT char *symbol_prefix = "linuxInput_";
 
+static int linuxInput_open_with_name(const char *device, const char *name)
+{
+	char buf[256];
+	int err, fd;
+
+	fd = open(device, O_RDONLY | O_NONBLOCK);
+	if (fd == -1)
+		return -1;
+
+	err = ioctl(fd, EVIOCGNAME(256), buf);
+	if (err == -1) {
+		close(fd);
+		return -1;
+	}
+	/* The kernel does not add a 0 terminator if the name is too long */
+	buf[255] = 0;
+
+	if (strcmp(buf, name)) {
+		/* Not the device we are looking for */
+		close(fd);
+		return -1;
+	}
+
+	return fd;
+}
+
+static int linuxInput_search_by_name(const char *name)
+{
+	char devname[PATH_MAX];
+	struct dirent *dirent;
+	int fd = -1;
+	DIR *dir;
+
+	dir = opendir("/dev/input");
+	if (dir == NULL)
+		return -1;
+
+	while ((dirent = readdir(dir)) != NULL) {
+		if (dirent->d_type != DT_CHR ||
+		    strncmp(dirent->d_name, "event", 5))
+			continue;
+
+		strcpy(devname, "/dev/input/");
+		strcat(devname, dirent->d_name);
+
+		fd = linuxInput_open_with_name(devname, name);
+		if (fd != -1)
+			break;
+	}
+
+	closedir(dir);
+
+	return fd;
+}
 
 /**
  * Initialize the driver.
@@ -114,10 +170,19 @@ linuxInput_init (Driver *drvthis)
 	report(RPT_INFO, "%s: using Device %s", drvthis->name, s);
 
 
-	if ((p->fd = open(s, O_RDONLY | O_NONBLOCK)) < 0) {
-		report(RPT_ERR, "%s: open(%s) failed (%s)",
-				drvthis->name, s, strerror(errno));
-		return -1;
+	/* Open the device, eiher by path or by name */
+	if (s[0] == '/') {
+		if ((p->fd = open(s, O_RDONLY | O_NONBLOCK)) == -1) {
+			report(RPT_ERR, "%s: open(%s) failed (%s)",
+					drvthis->name, s, strerror(errno));
+			return -1;
+		}
+	} else {
+		if ((p->fd = linuxInput_search_by_name(s)) == -1) {
+			report(RPT_ERR, "%s: could not find '%s' input-device",
+					drvthis->name, s);
+			return -1;
+		}
 	}
 
 	for (i = 0; (s = drvthis->config_get_string(drvthis->name, "key", i, NULL)) != NULL; i++) {
